94633d4fc1e84fd9ba529c9b2294b7f71e248eae
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-10-20 02:46:46 +0300

    add organization

---
+++ ticket-oraganization/src/main/java/com/ticket/organization/TicketOraganizationApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketOraganizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketOraganizationApplication.class, args);
+    }
+
+}
